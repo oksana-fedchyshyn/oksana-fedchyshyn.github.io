33f44dbfdd8e93b12210b3511c4e687cacce6054
oksana-fedchyshyn <49946329+oksana-fedchyshyn@users.noreply.github.com>
2019-05-04 18:58:38 +0300

    Add files via upload

---
+++ js/nav-scroll.js
@@ -0,0 +1,11 @@
+// navigation scroll
+$('.nav-link').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 0;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
+ 
